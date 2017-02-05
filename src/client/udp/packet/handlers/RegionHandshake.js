cdd1aaf856e0c609296fb67983505333ada4ad72
gwigz <contact.me@gwi.gr>
2017-02-05 13:53:18 +0000

    Added missing handling for RegionHandshakeReply

--- src/client/udp/packet/handlers/RegionHandshake.js
+++ src/client/udp/packet/handlers/RegionHandshake.js
@@ -1,8 +1,20 @@
 const AbstractHandler = require('./AbstractHandler')
+const PKID = require('../../../../utilities/Packets')
 
 class RegionHandshake extends AbstractHandler {
   handle() {
-    return false
+    const agent = this.manager.client.agent
+
+    this.manager.send(PKID.RegionHandshakeReply, {
+      agentData: {
+        agent: agent.id,
+        session: agent.session
+      },
+      regionInfo: {
+        // Set to support self appearance.
+        flags: 0x0100
+      }
+    })
   }
 }
 
